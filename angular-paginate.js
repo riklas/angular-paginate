fc53fc69d93e090861e53543217c162863f4ea5b
rikin shah <rikin_daya_s@hotmail.com>
2014-09-10 14:17:22 +0100

    create-pages directive complete

--- angular-paginate.js
+++ angular-paginate.js
@@ -1,81 +1,117 @@
 angular.module("angular-paginate", [])
 
+.controller("MainController", ["$scope", function($scope){
+    
+    $scope.pages = [];
+    $scope.pageContent = [];
+    // $scope.pc = []
+
+    $scope.getResults = function(){
+        $scope.results = [{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2},{name: 'a', avg: 2}];    
+    }
+    
+}])
+
 .controller("PaginationController", ["$scope", function($scope){
-    $scope.pages = [1,2,3,4,5];
-    var results = [];
-    $scope.numberPerPage = 15;
-    var currentPage = 1;    
+   
     
-    $scope.selectPage = function(pageNumber){
-        $scope.currentPage = pageNumber;
-        $scope.pageContent = paginationService.getPageContent($scope.currentPage);
-    };
+    // $scope.pageContent = [];
 
-    $scope.nextPage = function(){
-        if(isLastPage()){
-            $scope.currentPage++;
-            $scope.pageContent = paginationService.getPageContent($scope.currentPage);
-        }
-    };
+    // params : object with results, numberPerPage, page attributes
+    $scope.getPageContent = function(params){
 
-    $scope.previousPage = function(){
-        if(currentPage === 1){
-            $scope.currentPage--;
-            $scope.pageContent = paginationService.getPageContent($scope.currentPage);
-        }
+        var startIndex = params.numberPerPage * params.page - params.numberPerPage;
+        var newContent = [];
+        
+        if(params.results){
+
+            // if there are less results than number per page, display all results in pageContent
+            if((startIndex + params.numberPerPage - 1) > params.results.length){
+                for(var i = startIndex; i < params.results.length; i++){
+                    newContent.push(params.results[i]);
+                }
+            } else {
+                for(var i = startIndex; i < (params.numberPerPage + startIndex); i++){
+                    newContent.push(params.results[i]);
+                };
+            };
+
+            return newContent;            
+        };
     };
     
-    $scope.isLastPage = function(){
-        return (currentPage + 1 > pageNumbers[pageNumbers.length - 1]) ? true : false
-    };
+    // $scope.selectPage = function(pageNumber){
+    //     $scope.currentPage = pageNumber;
+    //     $scope.pageContent = paginationService.getPageContent($scope.currentPage);
+    // };
 
-    $scope.getPages = function(){
-        return paginationService.getPages();
-    };
+    // $scope.nextPage = function(){
+    //     if(isLastPage()){
+    //         $scope.currentPage++;
+    //         $scope.pageContent = paginationService.getPageContent($scope.currentPage);
+    //     }
+    // };
+
+    // $scope.previousPage = function(){
+    //     if(currentPage === 1){
+    //         $scope.currentPage--;
+    //         $scope.pageContent = paginationService.getPageContent($scope.currentPage);
+    //     }
+    // };
+    
+    // $scope.isLastPage = function(){
+    //     return (currentPage + 1 > pageNumbers[pageNumbers.length - 1]) ? true : false
+    // };
+
+    // $scope.isFirstPage = function() {
+    //     return (currentPage === 1) ? true : false;
+    // };
+
+    // $scope.getPages = function(){
+    //     return paginationService.getPages();
+    // };
 
 
-    $scope.paginationInit = function(model, limit){
-        $scope.pageNumbers = createPages(model, limit);
-        $scope.pageContent = getPageContent(1);
-        $scope.currentPage = 1;
-    };
+    // $scope.paginationInit = function(model, limit){
+    //     $scope.pageNumbers = createPages(model, limit);
+    //     $scope.pageContent = getPageContent(1);
+    //     $scope.currentPage = 1;
+    // };
 
 }])
 
 
-.factory("paginationService", function(){
-    return {
-        isFirstPage: function(){
-            return (currentPage === 1) ? true : false;
-        }
-    };
-})
-
-.directive("createPagination", function(){
+.directive("createPages", function(){
     return {
         restrict: "A",
         scope: {
-            results: "&",
-            numberPerPage: "&",
+            results: "=",       
+            numberPerPage: "@",
             pages: "=",
-            pageLimit: "&",                  // optional: set the maximum page numbers to 15
-            binding: "@"                     // optional: default pagination creation on click event. set to bind to any other type of event
+            pageContent: "="
         },
+        controller: "PaginationController",
         link: function(scope, elem, attrs){
             elem.bind("click", function(){ 
-                currentPage = 1
+                scope.pages = [];
+                scope.pageContent = [];
+                var currentPage = 1;
                 
                 // gets last page number
-                var pageMax = Math.ceil(scope.results.length / scope.numberPerPage);
+                var pageMax = Math.ceil(scope.results.length / parseInt(scope.numberPerPage));
                
                 // sets maximum number of page numbers to 15 if option not provided
                 if(!scope.pageLimit) scope.pageLimit = 15;
                 pageMax = (pageMax > scope.pageLimit) ? scope.pageLimit : pageMax;
 
-                // array to store page numbers
+                // // array to store page numbers
                 for(var i = 1; i <= pageMax; i++){
-                    scope.pageNumbers.push(i);
+                        scope.pages.push(i);
                 };
+                
+                scope.pageContent = scope.getPageContent({results: scope.results, page: currentPage, numberPerPage: parseInt(scope.numberPerPage)})
+
+                scope.$apply();         // apply changes
             });
         }
     };
@@ -85,28 +121,23 @@ angular.module("angular-paginate", [])
     return {
         restrict: "A",
         scope: {
-            results: "&",  
+            results: "=",  
             page: "@",
-            numberPerPage: "&",
+            numberPerPage: "@",
             pageContent: "=",
+            pageLimit: "@",             // optional: set the maximum page numbers to 15
+            binding: "@",               // optional: default pagination creation on click event. set to bind to any other type of event
         },
+        controller: "PaginationController",
         link: function(scope, elem, attrs){
             elem.bind("click", function(){
-                console.log(scope.page);
-                var startIndex = scope.numberPerPage * scope.page - scope.numberPerPage;
-                scope.pageContent = [];
-                if(scope.results){
-                    // if there are less results than number per page, display all results in pageContent
-                    if((startIndex + scope.numberPerPage - 1) > scope.results.length){
-                        for(var i = startIndex; i < scope.results.length; i++){
-                            scope.pageContent.push(scope.results[i]);
-                        }
-                    } else {
-                        for(var i = startIndex; i < (scope.numberPerPage + startIndex); i++){
-                            scope.pageContent.push(scope.results[i]);
-                        };
-                    };
-                };
+                console.log(scope.pageContent);
+                a = [{name: 't', avg: 1}];
+                scope.pageContent = a.slice(0);
+                scope.$apply();
+                // scope.pageContent = scope.getPageContent({results: scope.results, page: scope.page, numberPerPage: parseInt(scope.numberPerPage)});
+                console.log(scope.pageContent);
+
             }); 
         }
     };
